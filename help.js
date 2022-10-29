3431b1109bc6d2d558f1ad78c3cd83dce4c2696e
skyzomanagement <116863289+skyzomanagement@users.noreply.github.com>
2022-10-29 13:01:04 +0700

    Update help.js

--- help.js
+++ help.js
@@ -15,7 +15,7 @@ return `▻►▻►▻►▻►▻►▻►▻►▻►
 │ *•* ${prefix}rules
 │ *•* ${prefix}listpc
 │ *•* ${prefix}listgc
-│ *•* ${prefix}naufalgntg
+│ *•* ${prefix}script
 │ *•* ${prefix}groupwa
 ╰─────────────────
 ▻►▻►▻►▻►▻►▻►▻►▻►
